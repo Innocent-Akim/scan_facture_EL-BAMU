7e6b2b34fc934bfe2ed4d69b4af8a7a300785abe
Akim <gentilakili98@gmail.com>
2023-03-04 11:28:26 +0200

    add bloc login

--- appscan/lib/main.dart
+++ appscan/lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:appscan/src/pages/auth/Login.dart';
+import 'dart:io';
 import 'package:appscan/src/utils/App.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'src/api/MyHttpOverrides.dart';
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   return runApp(const App());
 }
--- appscan/lib/src/utils/App.dart
+++ appscan/lib/src/utils/App.dart
@@ -1,8 +1,8 @@
+import 'package:appscan/src/bloc/login_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import '../pages/auth/Login.dart';
 import '../pages/home/Home.dart';
 import 'UtilsColot.dart';
@@ -18,29 +18,34 @@ class App extends StatelessWidget {
       systemNavigationBarDividerColor: Colors.transparent,
       systemNavigationBarIconBrightness: Brightness.light,
     ));
-    return GetMaterialApp(
-      // theme:AppFont. _lightTheme,
-      darkTheme: AppFont.darkTheme,
-      initialRoute: '/',
-      defaultTransition: Transition.native,
-
-      getPages: [
-        GetPage(name: '/', page: () => const Login()),
-        GetPage(name: '/home', page: () => const Home()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginBloc>(create: (context) => LoginBloc()),
       ],
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        dividerTheme: const DividerThemeData(space: 0),
-        fontFamily: AppFont.primaryFont,
-      ).copyWith(
-        pageTransitionsTheme: const PageTransitionsTheme(
-          builders: <TargetPlatform, PageTransitionsBuilder>{
-            TargetPlatform.android: ZoomPageTransitionsBuilder(),
-          },
+      child: GetMaterialApp(
+        // theme:AppFont. _lightTheme,
+        darkTheme: AppFont.darkTheme,
+        initialRoute: '/',
+        // defaultTransition: Transition.native,
+
+        getPages: [
+          GetPage(name: '/', page: () => const Login()),
+          GetPage(name: '/home', page: () => const Home()),
+        ],
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+          dividerTheme: const DividerThemeData(space: 0),
+          fontFamily: AppFont.primaryFont,
+        ).copyWith(
+          pageTransitionsTheme: const PageTransitionsTheme(
+            builders: <TargetPlatform, PageTransitionsBuilder>{
+              TargetPlatform.android: ZoomPageTransitionsBuilder(),
+            },
+          ),
         ),
+        home: const Login(),
       ),
-      home: const Login(),
     );
   }
 }
